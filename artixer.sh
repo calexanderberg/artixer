72ca85250089948111c1c3e6dd2d58ce668dbaa3
calexanderberg <carlalexanderberg@gmail.com>
2022-06-15 21:02:57 +0200

    Realized I didn't have to nest if statements, so I started rewriting some parts.

--- artixer.sh
+++ artixer.sh
@@ -2,6 +2,8 @@
 
 # A script for installing Artix linux, an Arch based linux distribution.
 
+endMessage="Thank you for using Artixer\nShutting down ... \n\nHave a great day"
+
 echo -e "Welcome to Artixer, an installer for Artix linux\n\n"
 echo -e "In just a couple of minutes you will be able to one up, the \"I use
 arch btw\" people \n\n"
@@ -14,10 +16,15 @@ if [[ $decision == "yes" || $decision == "y" ]]; then
 	
 	echo -e "Awesome, let's get this started!\n\n"
 	echo -e "Below is a list of your partitions, please select which one to use.\n\n"
+	lsblk	
+	echo ""
+	read mainPartition
+	echo -e "\n\nYou selected ${mainPartition}, is this correct?"
 
+	
+	
 else 
-	echo -e "You did not want to continue\n\n"
+	echo -e "\nYou did not want to continue\n"
+	echo -e "${endMessage}"
+	exit 1
 fi
-
-echo -e "Thank you for using Artixer\nShutting down\n\nHave a great day"
-exit 1
