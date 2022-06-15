bb6239b041065761fc8c9892b1808305f58e1818
calexanderberg <carlalexanderberg@gmail.com>
2022-06-15 15:38:36 +0200

    Created some text for the script, not complete yet. Will see how much I get done on my vacation

---
+++ artixer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# A script for installing Artix linux, an Arch based linux distribution.
+
+echo -e "Welcome to Artixer, an installer for Artix linux\n\n"
+echo -e "In just a couple of minutes you will be able to one up, the \"I use
+arch btw\" people \n\n"
+echo -e "Remember this will erase any and all of your files. So stop here if 
+you forgot something.\n\nAre you ready? [y/n]"
+
+read decision
+
+if [[ $decision == "yes" || $decision == "y" ]]; then
+	
+	echo -e "Awesome, let's get this started!\n\n"
+	echo -e "Below is a list of your partitions, please select which one to use.\n\n"
+
+else 
+	echo -e "You did not want to continue\n\n"
+fi
+
+echo -e "Thank you for using Artixer\nShutting down\n\nHave a great day"
+exit 1
