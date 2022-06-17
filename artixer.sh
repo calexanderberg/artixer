cfa4a9214411d4cf890bb0ab6b4b1b74e2aece32
calexanderberg <carlalexanderberg@gmail.com>
2022-06-17 17:40:18 +0200

    I am on vacation, but I can not help myself. Added some more logic to the script

--- artixer.sh
+++ artixer.sh
@@ -42,4 +42,16 @@ do
 
 done
 
-echo -e "\n\n\ntest"
+echo -e "Awesome! How large do you want your boot parition to be? (in mb btw)"
+read bootPart
+echo -e "And your swap parition?"
+read swapPart
+
+# Do all the fdisk stuff, can't be bothered now
+
+echo -e "Boot, swap, and root partition is done.\n\n"
+echo -e "Let's go over some software.\n\n"
+echo -e "Please select what init system to use"
+
+echo -e "Select 1 for openrc"
+# Make a variable for this instead otherwise it will be tedius
